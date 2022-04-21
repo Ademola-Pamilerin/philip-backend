b635af70a4e9d376c07c40a339e681833d15bdae
Ademola-Pamilerin <64588069+Ademola-Pamilerin@users.noreply.github.com>
2022-04-21 11:49:48 +0100

    Update index.js
    Testing the endPoints

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const https = require("https");
 
 const app = express();
 
+app.get("/", (req, res, next) => {
+  res.send("Ademola");
+  console.log("running");
+});
+
 const server = https.createServer(app);
 server.listen(process.env.PORT, () => {
   console.log("Port started");
